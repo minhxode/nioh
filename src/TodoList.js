26fe8c1def40064a5719ebcf078bf5579a19a1d3
minhxode <minhln@hotmail.com>
2021-06-02 01:36:24 +0700

    delete some files

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,11 +1,7 @@
 import React from 'react'
 import Todo from "./Todo"
-import {useSelector, useDispatch} from 'react-redux';
-import{increment, decrement} from './components/Redux/Actions'
 
 export default function TodoList( {todos , toggleTodo} ) {
-    const listCounter = useSelector(state => state.listCounter);
-    const dispatch = useDispatch();
     return (
         todos.map(todoElement => {
             return <Todo key  = {todoElement.id}  toggleTodo = {toggleTodo} todo = {todoElement} />
